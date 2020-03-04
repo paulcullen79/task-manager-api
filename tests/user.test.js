20a3371219d0b5dd8d390f8c60099052bf9baa6a
paulcullen79 <cullenpaul79@gmail.com>
2020-03-03 19:32:25 -0500

    jest--watch now working

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest')
 const app = require('../src/app')
 
-// test ('should sign up a new user', async () => {
-//     await request(app).post('/users').send({
-//         name: 'Bob',
-//         email: 'cullenpaul79@hotmail.com',
-//         password: 'mypassword123'
-//     }).expect(201)
+test ('should sign up a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Bob',
+        email: 'cullenpaul79@hotmail.com',
+        password: 'mypassword123'
+    }).expect(201)
 
-// })
+})
